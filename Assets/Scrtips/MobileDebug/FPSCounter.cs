730bb2acf9875b3db1caaee58879b55832df6edd
Warriorroq <warriorroq@gmail.com>
2022-08-11 20:34:57 +0200

    fps counter

---
+++ Assets/Scrtips/MobileDebug/FPSCounter.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+namespace MobileDebug
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        private float[] _fpsForLastUpdates;
+        private uint _updates;
+        private void Awake()
+        {
+            _fpsForLastUpdates = new float[100];
+        }
+
+        private void Update()
+        {
+            var fps = 1 / Time.deltaTime;
+            _updates++;
+            _fpsForLastUpdates[_updates % (_fpsForLastUpdates.Length + 1)] = fps;
+            _text.text = $"fps:{fps:0.0}\nAverage:{_fpsForLastUpdates.Sum()/_fpsForLastUpdates.Length:0.0}";
+        }
+    }
+}
